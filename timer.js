f1ea1ac31f6601ee75ac7e9bb39555ddeb862e48
prokittycoder2019 <61248048+prokittycoder2019@users.noreply.github.com>
2020-02-19 21:31:11 +0200

    create timer js

---
+++ timer.js
@@ -0,0 +1,25 @@
+function padZero(n){
+  s=""+n;
+  if (s.length < 2) return ("0"+s);
+  return s;
+}
+
+function timer() {
+  dayStart = 268;
+  hrStart = 12;
+  minStart = 0;
+  secStart = 0;
+  
+  clockRate = 1;
+  
+  totalStartSec = ((dayStart*24 + hrStart)*60 + minStart)*60 + secStart;
+  curTotalSec = totalStartSec - Math.round((time - inPoint)*clockRate);
+  
+  curDay = Math.floor(curTotalSec/(24*3600));
+  curHr = Math.floor((curTotalSec%(24*3600))/3600);
+  curMin = Math.floor((curTotalSec%3600)/60);
+  curSec = curTotalSec%60;
+  leftTime = padZero(curDay) + ":" + padZero(curMin) + ":" + padZero(curSec);
+  document.getElementById("timer").innerHTML = leftTime;
+}
+var myTimer = setInterval(timer,1000);
